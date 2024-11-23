bfe53fa9f85cd24ad952172a391653cffc9bea5b
Barathbence <barathbence1@gmail.com>
2024-11-23 15:56:28 +0100

    commented on the bugs

--- main.cpp
+++ main.cpp
@@ -4,23 +4,41 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    // HIBA: A "NELEMENTS" helyett "N_ELEMENTS" kellene, mivel a változó neve máshogy van definiálva.
+    int *b = new int[NELEMENTS]; // Javítás: int *b = new int[N_ELEMENTS];
+
+    // HIBA: Az aposztróf (') helyett idézõjel (") kell a stringhez.
+    std::cout << '1-100 ertekek duplazasa' // Javítás: std::cout << "1-100 ertekek duplazasa";
+
+    // HIBA: A for-ciklus feltétele hibás. A feltétel hiányzik, és az inicializáló szakasz nem megfelelõ.
+    for (int i = 0;) // Javítás: for (int i = 0; i < N_ELEMENTS; i++)
     {
-        b[i] = i * 2;
+        b[i] = i * 2; // Ez rendben van.
     }
-    for (int i = 0; i; i++)
+
+    // HIBA: A második for-ciklus feltételének értéke mindig hamis (i; helyett i < N_ELEMENTS kell).
+    for (int i = 0; i; i++) // Javítás: for (int i = 0; i < N_ELEMENTS; i++)
     {
-        std::cout << "Ertek:"
-    }    
+        // HIBA: Hiányzik a pontosvesszõ (;) a "std::cout" végérõl.
+        std::cout << "Ertek:"; // Javítás: std::cout << "Ertek:" << b[i] << std::endl;
+    }
+
+    // HIBA: Hiányzik az int inicializálása. Az "atlag" változó nem inicializált, ami hibás számításokat okoz.
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag = 0; // Javítás: adjunk neki kezdeti értéket, például 0.
+    for (int i = 0; i < N_ELEMENTS, i++) // HIBA: A vesszõ (,) helyett pontosvesszõ (;) kell.
     {
-        atlag += b[i]
+        // HIBA: Hiányzik a pontosvesszõ (;) a sor végérõl.
+        atlag += b[i] // Javítás: atlag += b[i];
     }
+
+    // HIBA: Ha egész szám oszt egész számot, az eredmény levágott (int). Ez helyes, ha egész átlagot szeretnénk.
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
+
+    // HIBA: A "delete" nem került meghívásra a dinamikusan lefoglalt memória felszabadítására.
+    delete[] b; // Javítás: adjuk hozzá a memória felszabadítását.
+
     return 0;
 }
+
